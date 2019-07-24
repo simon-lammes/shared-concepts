5150482b3d56eb45dd5566c4fcac4bb3f37c7c56
Simon Lammes <simon.lammes@cofinpro.de>
2019-07-24 23:41:05 +0200

    login & logout + profile picture gets displayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Component, OnInit } from '@angular/core';
@@ -13,12 +15,15 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent implements OnInit {
 
   pictureUrl$: Observable<string>;
+  displayName$: Observable<string>;
+  isLoggedIn$: Observable<boolean>;
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private afAuth: AngularFireAuth
+    private afAuth: AngularFireAuth,
+    private router: Router
   ) {
     this.initializeApp();
   }
@@ -30,10 +35,22 @@ export class AppComponent implements OnInit {
     });
   }
 
-  ngOnInit() {   
+  ngOnInit() {  
+    this.pictureUrl$ = this.afAuth.authState.pipe(map(user =>{
+      return user ? user.photoURL: null;
+    }));
+    this.displayName$ = this.afAuth.authState.pipe(map(user =>{
+      return user ? user.displayName: null;
+    }));
+    this.isLoggedIn$ = this.afAuth.authState.pipe(map(user => !!user));
   }
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  login() {
+    sessionStorage.setItem('redirectUrl', this.router.url);
+    this.router.navigateByUrl('/auth');
   }
 }
