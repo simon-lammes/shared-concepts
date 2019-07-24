f75366ea945ea1d4ea2e542b221a68e4a0fd1854
Simon Lammes <simon.lammes@cofinpro.de>
2019-07-24 22:34:35 +0200

    init firebase storage and firebase hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDZzfDH-gGNEvTptOZkmJLLfFpx2rRhoz8",
+    authDomain: "shared-concepts.firebaseapp.com",
+    databaseURL: "https://shared-concepts.firebaseio.com",
+    projectId: "shared-concepts",
+    storageBucket: "shared-concepts.appspot.com",
+    messagingSenderId: "76627842867",
+    appId: "1:76627842867:web:191f1574b6e054a0"
+  }
 };
