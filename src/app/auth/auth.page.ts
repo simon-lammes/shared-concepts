49dfc8793f4ea4b6a6de7ab63424057d6b4a7925
Simon Lammes <simon.lammes@cofinpro.de>
2019-08-08 14:29:09 +0200

    removed unnecessary user service

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,59 +1,57 @@
-import { Component, OnInit, NgZone, OnDestroy } from '@angular/core';
+import {Component, NgZone, OnDestroy, OnInit} from '@angular/core';
 import * as firebase from 'firebase/app';
 import * as firebaseui from 'firebaseui';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
-import { UserService } from '../user/user.service';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+    selector: 'app-auth',
+    templateUrl: './auth.page.html',
+    styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit, OnDestroy {
 
-  ui: firebaseui.auth.AuthUI;
-  uiConfig: any;
+    ui: firebaseui.auth.AuthUI;
+    uiConfig: any;
 
-  constructor(
-    private afAuth: AngularFireAuth,
-    private router: Router,
-    private ngZone: NgZone,
-    private userService: UserService
-  ) { }
-
-  ngOnDestroy() {
-    this.ui.delete();
-  }
-
-  ngOnInit() {
-    this.uiConfig = {
-      signInOptions: [
-        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        firebase.auth.EmailAuthProvider.PROVIDER_ID
-      ],
-      callbacks: {
-        signInSuccessWithAuthResult: this
-          .onLoginSuccessful
-          .bind(this)
-      }
+    constructor(
+        private afAuth: AngularFireAuth,
+        private router: Router,
+        private ngZone: NgZone,
+    ) {
     }
-    this.ui = new firebaseui.auth.AuthUI(this.afAuth.auth);
-  }
 
-  ionViewWillEnter() {
-    this.ui.start('#auth-container', this.uiConfig);
-  }
+    ngOnDestroy() {
+        this.ui.delete();
+    }
 
-  onLoginSuccessful() {
-    this.router.navigateByUrl('/concepts');
-    // The redirectUrl has to be stored in session storage.
-    // Otherwise it would get lost during authentication using Google
-    const redirectUrl = sessionStorage.getItem('redirectUrl');
-    this.userService.createUserInDbIfNotYetExisting().subscribe();
-    // Angular cannot know by itself that this method changes the view, 
-    // so we need to execute it inside the angular zone
-    this.ngZone.run(() => this.router.navigateByUrl(redirectUrl));
-  }
+    ngOnInit() {
+        this.uiConfig = {
+            signInOptions: [
+                firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                firebase.auth.EmailAuthProvider.PROVIDER_ID
+            ],
+            callbacks: {
+                signInSuccessWithAuthResult: this
+                    .onLoginSuccessful
+                    .bind(this)
+            }
+        };
+        this.ui = new firebaseui.auth.AuthUI(this.afAuth.auth);
+    }
+
+    ionViewWillEnter() {
+        this.ui.start('#auth-container', this.uiConfig);
+    }
+
+    onLoginSuccessful() {
+        this.router.navigateByUrl('/concepts');
+        // The redirectUrl has to be stored in session storage.
+        // Otherwise it would get lost during authentication using Google
+        const redirectUrl = sessionStorage.getItem('redirectUrl');
+        // Angular cannot know by itself that this method changes the view,
+        // so we need to execute it inside the angular zone
+        this.ngZone.run(() => this.router.navigateByUrl(redirectUrl));
+    }
 
 }
