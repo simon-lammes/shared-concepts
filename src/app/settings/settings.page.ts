0635dc4e16b3086dc2ef4c077ef80fd0374bf9cb
Simon Lammes <simon.lammes@cofinpro.de>
2019-08-18 21:41:54 +0200

    Added option to remove concepts from cache

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {IonSelect, ModalController} from '@ionic/angular';
+import {AlertController, IonSelect, ModalController} from '@ionic/angular';
 import {HelpModalComponent} from '../help-modal/help-modal.component';
 import {HelpSection} from '../help-modal/help-section.model';
 import {Observable} from 'rxjs';
@@ -20,7 +20,8 @@ export class SettingsPage implements OnInit {
 
     constructor(
         private modalController: ModalController,
-        private settingsService: SettingsService
+        private settingsService: SettingsService,
+        private alertController: AlertController
     ) {
     }
 
@@ -35,6 +36,11 @@ export class SettingsPage implements OnInit {
                 helpText: 'The cooldown time describes for how long an exercise ' +
                     'cannot be presented to you after you have seen the solution. ' +
                     'Its purpose is to diversify your learning experience.'
+            },
+            {
+                topic: 'Clear Cache',
+                helpText: 'Clearing the cache helps when the concepts on the server have changed and you want to get those changes. ' +
+                    'By clearing the cache, all concepts have to be fetched again and thereby you get the new concepts.'
             }
         ];
         this.modalController.create({
@@ -61,6 +67,27 @@ export class SettingsPage implements OnInit {
     getArrayWithNumbersFromZeroToN(n: number): number[] {
         // => [0,1,2,3...n]
         return Array.from(Array(n + 1).keys());
+    }
+
+    onClearCacheRequested() {
+        this.alertController.create({
+            header: 'Clear Cache',
+            message: 'When you clear the cache, all concepts have to be fetched again from the server. ' +
+                'Nevertheless, this is not a dangerous operation. ' +
+                'This operation is useful when you want to fetch the newest concepts from the server.',
+            buttons: [
+                {
+                    text: 'remove concepts from cache',
+                    handler: () => {
+                        localStorage.removeItem('@@STATE');
+                    }
+                },
+                {
+                    text: 'cancel',
+                    role: 'cancel'
+                }
+            ]
+        }).then(toastElement => toastElement.present());
     }
 }
 
