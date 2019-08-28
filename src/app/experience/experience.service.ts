f10404e31244e194ba14f7ec14f8d342cdcd7edc
Simon Lammes <simon.lammes@cofinpro.de>
2019-08-29 00:39:51 +0200

    fixed migration bug

--- src/app/experience/experience.service.ts
+++ src/app/experience/experience.service.ts
@@ -1,11 +1,10 @@
-import {Injectable} from '@angular/core';
+import {Injectable, OnDestroy} from '@angular/core';
 import {AngularFirestore} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
 import {ExperienceMap} from './experience.model';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {map, switchMap} from 'rxjs/operators';
 import {untilDestroyed} from 'ngx-take-until-destroy';
-import {OnDestroy} from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Injectable({
     providedIn: 'root'
